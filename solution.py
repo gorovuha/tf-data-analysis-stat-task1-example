a211a4ba21d73a037b7d3daa53b8d1d70f817d68
gorovuha <49656254+gorovuha@users.noreply.github.com>
2023-04-07 16:30:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,8 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    sum = 0
+    for i in range(n):
+        sum += (i+1)*x[i]
+    return sum/n
